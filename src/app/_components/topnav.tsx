0d44fefc951aea3f6426c9200b36f3df651227df
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2025-07-14 02:39:37 +0500

    upload button changed

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,22 +1,18 @@
-"use client";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "../../utils/uploadthing";
-import { useRouter } from "next/navigation";
+import {SimpleUploadButton} from "./simple-upload-button";
 
 export function TopNav() {
-  const router = useRouter();
+  
   return (
     <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-4 items-center">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         {/* This button will only render if the user is signed in */}
         <SignedIn>
-          <UploadButton endpoint="imageUploader"  onClientUploadComplete={()=>{
-            router.refresh();
-          }}/>
+          <SimpleUploadButton  />
           <UserButton />
         </SignedIn>
       </div>
